bf850b1882a606d7ef6c8ad1155b9e107de92c37
Lisand <boevladik@gmail.com>
2023-07-12 12:07:41 +0300

    add setPlacementCardinates()

--- LearnJS/[2.1] - [2.18] Основы JS/[2.12] Оператор нулевого слияния/Оператор нулевого слияния.js
+++ LearnJS/[2.1] - [2.18] Основы JS/[2.12] Оператор нулевого слияния/Оператор нулевого слияния.js
@@ -10,4 +10,27 @@ console.log("Пустая строка:", user3 ?? "аноним");
 let user4 = "0";
 console.log("Строка 0:", user4 ?? "аноним");
 
+function validation(user) {
+  return user ?? "Гость";
+}
+
+// function setPlacementCardinates(src, x = 0, y = 0) {} аналогичный способ
+
+function setPlacementCardinates(src, x, y) {
+    
+  if (!src) {
+    throw new Error("src undefined");
+  }
+
+  const img = new Image();
+
+  img.onload = function () {
+    ctx.drawImage(img, y ?? 0, x ?? 0);
+  };
+
+  console.log(x, y);
+}
+
+setPlacementCardinates();
+
 // Применение: установка значения по умолчанию
