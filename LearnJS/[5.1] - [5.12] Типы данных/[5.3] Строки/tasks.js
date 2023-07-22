c2d568af3dbabb44a7ff8d25f2e731046f5dab79
Lisand <boevladik@gmail.com>
2023-07-22 22:07:14 +0300

    [5.2] - [5.4]

---
+++ LearnJS/[5.1] - [5.12] Типы данных/[5.3] Строки/tasks.js
@@ -0,0 +1,78 @@
+/*
+
+  Напишите функцию ucFirst(str), возвращающую строку str с заглавным первым символом. Например:
+  ucFirst("вася") == "Вася";
+
+*/
+
+function ucFirst(str) {
+  return (newStr = str[0].toUpperCase() + str.slice(1));
+}
+
+/***********************************************/
+
+/*
+
+  Напишите функцию checkSpam(str), возвращающую true, если str содержит 
+  'viagra' или 'XXX', а иначе false.
+  
+  Функция должна быть нечувствительна к регистру:
+  checkSpam('buy ViAgRA now') == true
+  checkSpam('free xxxxx') == true
+  checkSpam("innocent rabbit") == false
+
+*/
+
+function checkSpam(str) {
+  str.toLowerCase;
+  return str.includes("viagra") || str.includes("xxx");
+}
+
+/***********************************************/
+
+/*
+
+ Создайте функцию truncate(str, maxlength), которая проверяет длину строки str и, 
+ если она превосходит maxlength, заменяет конец str на "…", так, чтобы её длина 
+ стала равна maxlength.
+ 
+ Результатом функции должна быть та же строка, если усечение не требуется, либо, 
+ если необходимо, усечённая строка.
+
+ Например:
+ truncate("Вот, что мне хотелось бы сказать на эту тему:", 20) = "Вот, что мне хотело…"
+ truncate("Всем привет!", 20) = "Всем привет!"
+  
+*/
+
+function truncate(str, maxlength) {
+  if (str.length < maxlength) {
+    return str;
+  }
+
+  if (str.length > maxlength) {
+    return str.substring(0, maxlength) + "…";
+  }
+}
+
+/***********************************************/
+
+/*
+
+  Есть стоимость в виде строки "$120". То есть сначала идёт знак валюты,
+  а затем – число.
+  
+  Создайте функцию extractCurrencyValue(str), которая будет из такой строки 
+  выделять числовое значение и возвращать его.
+  
+  Например:
+  alert( extractCurrencyValue('$120') === 120 ); // true
+
+*/
+
+function extractCurrencyValue(str) {
+  if (str[0] === "$") {
+    return str.slice(1);
+  }
+  return str;
+}
