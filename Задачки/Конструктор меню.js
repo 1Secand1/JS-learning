f53f512916645401eeb73f1881f8f8085aa0b57c
Lisand <boevladik@gmail.com>
2023-08-14 17:21:37 +0300

    abo

--- Задачки/Конструктор меню.js
+++ Задачки/Конструктор меню.js
@@ -1,75 +1,120 @@
-let l01 = new CreateОrder("Влад");
-
 function CreateОrder() {
   const list = new Map();
 
   this.lock = false;
   this.list = list;
+
   this.addItem = function (item, count) {
-    item = item.toLocaleLowerCase();
-
-    if (!isFinite(count)) {
-      throw new Error("Передано не числовое значение");
+    if (this.lock) {
+      console.log("Изменение списака заблакированно");
+      return;
     }
 
-    if (count <= 0) {
-      throw new Error("Передано отрицательне число");
+    let { name, unitPrice } = item;
+    name = name.toLocaleLowerCase();
+
+    countValidation(count);
+
+    if (list.has(name)) {
+      const element = list.get(name);
+
+      list.set(name, {
+        unitPrice: element.unitPrice,
+        count: element.count + count,
+      });
+      return;
     }
 
-    if (list.has(item)) {
-      list.set(item, list.get(item) + count);
+    if (!unitPrice) {
+      throw new Error("Ну указанно цена за штуку");
     }
 
-    if (!list.has(item)) {
-      list.set(item, count);
-    }
-  };
-  this.removeItem = function (item, count) {
-    item = item.toLocaleLowerCase();
-
-    if (!isFinite(count)) {
-      throw new Error("Передано не числовое значение");
-    }
-
-    if (count <= 0) {
-      throw new Error("Передано отрицательне число");
-    }
-
-    if (!list.has(item.toLocaleLowerCase())) {
-      throw new Error("Данная позиция не сущиствует в списке");
-    }
-
-    if (list.has(item)) {
-      list.set(item, list.get(item) - count);
-    }
-
-    if (list.get(item) === 0) {
-      list.delete(item);
+    if (!list.has(name)) {
+      list.set(name, { unitPrice, count });
     }
   };
+
+  this.removeItem = function (item, count = 0) {
+    if (this.lock) {
+      console.log("Изменение списака заблакированно");
+      return;
+    }
+
+    let { name, unitPrice } = item;
+    name = name.toLocaleLowerCase();
+    const element = list.get(name);
+
+    if (element.count - count <= 0 || count == 0) {
+      list.delete(name);
+      return;
+    }
+
+    countValidation(count);
+
+    if (list.has(name)) {
+      list.set(name, {
+        unitPrice: element.unitPrice,
+        count: element.count - count,
+      });
+    }
+  };
+
   this.getCheck = function () {
-    list.forEach((e, k) => {
-      console.log(`${k} - ${e} руб `);
+    let totalPrice = 0;
+    list.forEach((element, key) => {
+      const a = element.unitPrice * element.count;
+      totalPrice += a;
+
+      console.log(`${element.count} ${key} - ${a}р`);
     });
+    console.log(`Общая цена : ${totalPrice}`);
   };
 
   this.lockOrder = function () {
     Object.defineProperty(this, "lock", {
-      value: "false",
+      value: false,
     });
   };
   this.unlockOrder = function () {
     Object.defineProperty(this, "lock", {
-      value: "true",
+      value: true,
     });
   };
   Object.defineProperty(this, "lock", {
     writable: this.lock,
   });
+
+  function countValidation(count) {
+    if (!count) {
+      throw new Error("Не переданно число товоаров");
+    }
+
+    if (!isFinite(count)) {
+      throw new Error("Передано не числовое значение");
+    }
+
+    if (count !== Math.floor(count)) {
+      throw new Error("Передано не целое число");
+    }
+
+    if (count <= 0) {
+      throw new Error("Передано отрицательне число");
+    }
+  }
 }
 
 let user1 = new CreateОrder();
-user1.addItem("Чай", 100);
-user1.addItem("Кофе", 80);
-user1.addItem("Питца", 200);
+
+user1.addItem({ name: "Вафля", unitPrice: 100 }, 1);
+user1.addItem({ name: "Вафля" }, 1);
+user1.addItem({ name: "пивО", unitPrice: 100 }, 1);
+user1.addItem({ name: "пиво", unitPrice: 100 }, 1);
+user1.removeItem({ name: "Вафля" }, 1);
+
+user1.lockOrder();
+user1.addItem({ name: "Абоба чай", unitPrice: 100 }, 1);
+
+user1.unlockOrder();
+user1.addItem({ name: "Абоба чай", unitPrice: 100 }, 1);
+
 user1.getCheck();
