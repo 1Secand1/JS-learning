2fa1c93dd86b72f7924e6ce526cf9c9a908f35ef
1Secand1 <boevladik@gmail.com>
2023-11-05 09:16:58 +0300

    Прототипное наследование

--- LearnJS/[8.1] - [8.4] Прототипы, наследование/[8.1] Прототипное наследование/tasks.js
+++ LearnJS/[8.1] - [8.4] Прототипы, наследование/[8.1] Прототипное наследование/tasks.js
@@ -55,7 +55,7 @@ if (0) {
 }
 
 //****************************************** */
-if (1) {
+if (0) {
   // Какой объект получит свойство full при вызове rabbit.eat(): animal или rabbit?
   let animal = {
     eat() {
@@ -69,4 +69,39 @@ if (1) {
 
   rabbit.eat();
   //свойско full получит объект rabbit
+}
+
+//****************************************** */
+if (1) {
+  /**
+   * У нас есть два хомяка: шустрый (speedy) и ленивый (lazy); оба наследуют от общего объекта hamster.
+   * Когда мы кормим одного хомяка, второй тоже наедается. Почему? Как это исправить?
+   */
+
+  let hamster = {
+    stomach: [],
+
+    eat(food) {
+      this.stomach.push(food);
+    },
+  };
+
+  let speedy = {
+    stomach: [], // add stomach
+
+    __proto__: hamster,
+  };
+
+  let lazy = {
+    stomach: [], // add stomach
+
+    __proto__: hamster,
+  };
+
+  // Этот хомяк нашёл еду
+  speedy.eat("apple");
+  console.log(speedy.stomach); // apple
+
+  // У этого хомяка тоже есть еда. Почему? Исправьте
+  console.log(lazy.stomach); // apple
 }
