9d8f99052b9d9a0a1302bdc5c707fb3d1079ee47
1Secand1 <boevladik@gmail.com>
2023-11-05 09:54:45 +0300

    Встроенные прототипы

--- LearnJS/[8.1] - [8.4] Прототипы, наследование/[8.3] Встроенные прототипы/tasks.js
+++ LearnJS/[8.1] - [8.4] Прототипы, наследование/[8.3] Встроенные прототипы/tasks.js
@@ -1,13 +1,35 @@
-/**
- * Добавьте всем функциям в прототип метод defer(ms), который вызывает функции через ms миллисекунд.
- */
+if (0) {
+  /**
+   * Добавьте всем функциям в прототип метод defer(ms), который вызывает функции через ms миллисекунд.
+   */
 
-Function.prototype.defer = function (ms) {
-  setTimeout(this, ms);
-};
+  Function.prototype.defer = function (ms) {
+    setTimeout(this, ms);
+  };
 
-function f() {
-  console.log("Hello!");
+  function f() {
+    console.log("Hello!");
+  }
+
+  f.defer(1000);
 }
 
-f.defer(1000);
+if (1) {
+  /**
+   * Добавьте всем функциям в прототип метод defer(ms), который возвращает обёртку,
+   * откладывающую вызов функции на ms миллисекунд.
+   */
+
+  Function.prototype.defer = function (ms) {
+    let fun = this;
+    return function (...args) {
+      setTimeout(() => fun.apply(this, args), ms);
+    };
+  };
+
+  function f(a, b) {
+    console.log(`${a} + ${b} = ${a + b}`);
+  }
+
+  f.defer(1000)(1, 2); // выведет 3 через 1 секунду.
+}
