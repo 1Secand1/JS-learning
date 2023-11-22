1c713e6d729aacb0b2e7ab307643f9cd703c3953
1Secand1 <boevladik@gmail.com>
2023-11-22 11:10:42 +0300

    Конструктор меню на классах

--- Задачки/конструктор меню/main.js
+++ Задачки/конструктор меню/main.js
@@ -1,9 +1,18 @@
 class MenuList {
   constructor() {
     this.list = new Map();
+    this._editable = true;
+  }
+
+  get canBeEdited() {
+    return this._editable;
   }
 
   addItem(item, count) {
+    if (!this._editable) {
+      throw new Error("Изменение запрещено");
+    }
+
     if (typeof item !== "object") {
       throw new Error("item ожидался типом object");
     }
@@ -21,8 +30,6 @@ class MenuList {
 
     if (this.list.has(key)) {
       value = this.list.get(key);
-      value = JSON.parse(value);
-
       value.count = value.count + count;
     } else {
       value = {
@@ -31,14 +38,19 @@ class MenuList {
       };
     }
 
-    value = JSON.stringify(value);
     this.list.set(key, value);
   }
 
   removeItem(item, count = 0) {
+    if (!this._editable) {
+      throw new Error("Изменение запрещено");
+    }
+    if (typeof item !== "object") {
+      throw new Error("item ожидался типом object");
+    }
+
     const key = Object.keys(item)[0];
     let value = this.list.get(key);
-    value = JSON.parse(value);
 
     const difference = value.count - count;
 
@@ -53,17 +65,92 @@ class MenuList {
 
     value.count = value.count - count;
 
-    value = JSON.stringify(value);
     this.list.set(key, value);
   }
-  
-  getCheck() {}
-  lockOrder() {}
-  unlockOrder() {}
+
+  getCheck() {
+    let check = [];
+    let totalСost = 0;
+
+    this.list.forEach((e, key) => {
+      let amount = e.count * e.unitPrice;
+      check.push(`${key} ${e.count} шт - ${amount} рублей`);
+      totalСost += amount;
+    });
+
+    check.push(`Итого - ${totalСost} рублей`);
+
+    return check;
+  }
+  lockOrder() {
+    this._editable = false;
+  }
+  unlockOrder() {
+    this._editable = true;
+  }
 }
 
 let menuList = new MenuList();
 
-menuList.addItem({ курица: 100 }, "5");
-menuList.addItem({ курица: 100 }, "5");
-menuList.removeItem({ курица: 100 }, 5);
+const nameItemInput = document.getElementById("nameItem");
+const unitCostInput = document.getElementById("unitСost");
+const quantityInput = document.getElementById("quantity");
+
+function modifyMenuItem(
+  idButton,
+  inputName,
+  inputUnitCost,
+  inputQuantity,
+  action
+) {
+  const button = document.getElementById(idButton);
+
+  button.addEventListener("click", () => {
+    const item = {};
+    item[inputName.value] = inputUnitCost.value;
+
+    if (action === "add") {
+      menuList.addItem(item, inputQuantity.value);
+    } else if (action === "remove") {
+      menuList.removeItem(item, inputQuantity.value);
+    }
+
+    inputName.value = "";
+    inputUnitCost.value = "";
+    inputQuantity.value = "";
+  });
+}
+function printList(idHtmlElement, idButton) {
+  const button = document.getElementById(idButton);
+  const htmlElement = document.getElementById(idHtmlElement);
+  const textItemInList = menuList.getCheck();
+
+  button.addEventListener("click", () => {
+    let content = "";
+    textItemInList.forEach((item) => {
+      console.log(item);
+      content += `
+      <li>
+        ${item}
+      </li>
+      `;
+    });
+    htmlElement.innerHTML = content;
+  });
+}
+function editPermissionSwitch(idButtonLock, idButtonUnLock) {
+  const buttonLock = document.getElementById(idButtonLock);
+  const buttonUnLock = document.getElementById(idButtonUnLock);
+
+  buttonLock.addEventListener("click", () => {
+    menuList.lockOrder();
+  });
+  buttonUnLock.addEventListener("click", () => {
+    menuList.unlockOrder();
+  });
+}
+
+modifyMenuItem("add", nameItemInput, unitCostInput, quantityInput, "add");
+modifyMenuItem("remove", nameItemInput, unitCostInput, quantityInput, "remove");
+editPermissionSwitch("lockOrder", "unlockOrder");
+printList("check", "summarize");
