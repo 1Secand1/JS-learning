e27773f960e7638d03bfc04980e77381f3e246d2
1Secand1 <boevladik@gmail.com>
2023-12-01 18:57:00 +0300

    add AccessLevelСonstructor

--- LearnJS/[9.1] - [9.7] Классы/[9.3]Статические свойства и методы.js
+++ LearnJS/[9.1] - [9.7] Классы/[9.3]Статические свойства и методы.js
@@ -32,3 +32,69 @@ let moderator = admin.createNewRole("moderator123", "admin12");
 console.log(superAdmin);
 console.log(admin);
 console.log(moderator);
+
+//______________________________________________________________________
+
+class AccessLevelСonstructor {
+  constructor(roleName, capabilities) {
+    if (AccessLevelСonstructor.roleList) {
+      AccessLevelСonstructor.accessLevel++;
+    }
+
+    AccessLevelСonstructor.roleList[roleName] = {
+      accessLevel: AccessLevelСonstructor.accessLevel,
+      methods: capabilities,
+      assignRole: AccessLevelСonstructor.getRole,
+    };
+  }
+
+  static accessLevel = 0;
+  static roleList = {};
+
+  static AccessLevelList = {};
+
+  static getRole(roleName, password, providedPassword) {
+    if (this !== AccessLevelСonstructor) {
+      let roleAccessLevel =
+        AccessLevelСonstructor.roleList[roleName].accessLevel;
+
+      if (
+        this.accessLevel > roleAccessLevel ||
+        this.password !== providedPassword
+      ) {
+        throw new Error("Недостаточно прав доступа");
+      }
+    }
+
+    return {
+      password,
+      ...AccessLevelСonstructor.roleList[roleName],
+    };
+  }
+}
+
+new AccessLevelСonstructor("admin", {
+  createPost() {
+    console.log("Пост создан");
+  },
+  deletePost() {
+    console.log("Пост Удолён");
+  },
+});
+
+new AccessLevelСonstructor("moder", {
+  editPost() {
+    console.log("Пост отредактирован");
+  },
+});
+
+new AccessLevelСonstructor("user", {
+  exist() {
+    console.log("Существует...");
+  },
+});
+
+const Vlad = AccessLevelСonstructor.getRole("admin", "admin123");
+
+const Dima = Vlad.assignRole("moder", "Dima123", "admin123");
+const aboba = Dima.assignRole("user", "aboba", "Dima123");
